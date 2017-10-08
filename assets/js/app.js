ea8ad4f2e17fdd2f717a7bb8bb4b10b1726bc22a
tkj@live.it <tkj@live.it>
2017-10-08 07:50:50 +0200

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,7 @@ channel.on("info", pl => {
 });
 
 channel.on("stato", pl => {
-  console.log(pl);
+  console.log('STATO', pl);
 
   new Chart(document.getElementById("chartjs-4b"), {
       type: "doughnut",
@@ -103,31 +103,10 @@ channel.on("stato", pl => {
           }]
       }
   });
-
-  new Chart(document.getElementById("chartjs-0s"), {
-    type: 'line',
-    data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [{
-          label: "My First Dataset",
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: "rgb(75, 192, 192)",
-          lineTension: 3
-      }, {
-          label: "My First Dataset",
-          data: [12, 43],
-          fill: false,
-          borderColor: "rgb(34, 65, 67)",
-          lineTension: 3
-      }]
-    },
-    options: {}
-  });
 });
 
-channel.on("lista", payload => {
-  console.log(payload);
+channel.on("lista", pl => {
+console.log('LISTA', pl);
 
   new Chart(document.getElementById("chartjs-0b"), {
     type: 'line',
